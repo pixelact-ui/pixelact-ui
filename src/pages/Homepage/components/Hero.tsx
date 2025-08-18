87c314781efa006ed51c7d4e8a2aa8ca712677f6
alburt <albert.asaftei00@gmail.com>
2025-08-18 09:41:18 +0200

    fix: :clown_face: remove test component

--- src/pages/Homepage/components/Hero.tsx
+++ src/pages/Homepage/components/Hero.tsx
@@ -44,17 +44,6 @@ const Hero = () => {
           Documentation
         </Button>
       </div>
-
-      <Select>
-        <SelectTrigger className="w-[180px]">
-          <SelectValue placeholder="Theme" />
-        </SelectTrigger>
-        <SelectContent>
-          <SelectItem value="light">Light</SelectItem>
-          <SelectItem value="dark">Dark</SelectItem>
-          <SelectItem value="system">System</SelectItem>
-        </SelectContent>
-      </Select>
     </section>
   );
 };
